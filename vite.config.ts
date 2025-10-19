9f495a157d2021bab930f8f80169d03af27c99e9
plaxotin <plaxotin9@gmail.com>
2025-10-20 01:53:48 +0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/need-it-find-it/' : '/',
+  base: mode === 'production' ? '/RevMarket/' : '/',
   server: {
     host: "::",
     port: 8080,
